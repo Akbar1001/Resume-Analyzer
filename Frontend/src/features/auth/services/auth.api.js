87df672b62279b303b47cbb63eaac2d326219ebb
Akbar <akbar.code20@gmail.com>
2026-04-03 18:27:08 +0530

    implementing CORS

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,11 +1,17 @@
+/* this is the code for api layer which helps in communication b/w the backend api and frontend */
+
 import axios from "axios"
+
+//creating an instance of axios for repetetive code
+const api= axios.create({
+    baseURL:"http://localhost:3000",
+    withCredentials:true
+})
 
 export async function register({username,email,password}){
     try{
-        const responce = await axios.post("http://localhost:3000/api/auth/register",{
+        const responce = await api.post("/api/auth/register",{
             username,email,password
-        },{
-            withCredentials:true
         })
 
         return responce.data;
@@ -18,10 +24,8 @@ export async function register({username,email,password}){
 
 export async function login({email,password}) {
     try{
-        const response = await axios.post("http://localhost:3000/api/auth/login",{
+        const response = await api.post("/api/auth/login",{
             email,password
-        },{
-            withCredentials:true
         })
 
         return response.data;
@@ -30,4 +34,30 @@ export async function login({email,password}) {
         console.log(err);
         
     }
+}
+
+export async function logout(){
+
+    try{
+
+        const response = await api.get("/api/auth/logout")
+
+        return response.data;
+    }catch(err){
+        console.log(err);
+        
+    }
+}
+
+export async function getMe() {
+
+    try{
+        const response= await api.get("/api/auth/get-me")
+        
+        return response.data; 
+
+    }catch(err){
+        console.log(err);
+    }
+    
 }
