39b88f88fa5771573826b293f3b76193f6dc51fc
Akbar <akbar.code20@gmail.com>
2026-04-09 06:55:52 +0530

    redeploy backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,6 +13,7 @@ const allowedOrigins = new Set([
     "http://localhost:5174",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
+    "https://resume-analyzer-krrc.vercel.app",
 ])
 
 app.use(cors({
